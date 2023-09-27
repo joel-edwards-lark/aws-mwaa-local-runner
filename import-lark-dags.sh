a0c23aa16ddc4baaaa2455e3e0c1a8a31cdce182
Joel Edwards <joel.edwards@lark.com>
2023-09-27 10:20:57 -0600

    feat: add export and scratch volumes

--- import-lark-dags.sh
+++ import-lark-dags.sh
@@ -1,20 +1,24 @@
 #!/bin/zsh
-export AIRFLOW_VERSION=1.10.12
-export PYTHON_VERSION=3.7.12
+export AIRFLOW_VERSION=2.6.3
+export PYTHON_VERSION=3.8.15
 LARK_AIRFLOW_DAG_BRANCH=rc-0.3
 LARK_SOLIDDAGS_BRANCH=develop
 
-mwaaDir=$PWD
+# Use the expected branches for soliddags and the DAGs
+set -e
 
-# ensure you are importing the right code
-cd ../data-airflow-dags-v2
+pushd ../data-airflow-dags-v2
 git checkout $LARK_AIRFLOW_DAG_BRANCH
 git pull
-cd ../airflow-soliddags
+popd
+
+pushd ../airflow-soliddags
 git checkout $LARK_SOLIDDAGS_BRANCH
 git pull
+popd
 
-cd $mwaaDir
+set +e
+
 #  copy the group of requirements files
 cp ../data-airflow-dags-v2/*.txt docker/config
 cp ../airflow-soliddags/pipenv-requirements.txt docker/config
@@ -22,6 +26,10 @@ cp ../airflow-soliddags/pipenv-requirements.txt docker/config
 #  Get the CodeArtifact token for soliddags:
 export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --profile=infra --domain lark --domain-owner 509967626473 --query authorizationToken --output text`
 echo "--extra-index-url https://aws:${CODEARTIFACT_AUTH_TOKEN}@lark-509967626473.d.codeartifact.us-east-1.amazonaws.com/pypi/data-eng-pypi/simple/" > docker/config/codeartifact_local.txt
+
+# Export data will land here
+mkdir -p data/exports/
+mkdir -p data/scratch/
 
 # This command will install the requirements from the file:
 mkdir dags/{config,airflow_resources,utils}
@@ -38,5 +46,4 @@ sed -i '' '1i\
 ' docker/config/requirements-local.txt
 sed -i '' '/soliddags/d' docker/config/requirements-local.txt
 sed -i '' '/soliddags/d' docker/config/requirements.txt
-
 
